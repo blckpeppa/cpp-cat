28be9c2edc0123981a686b004192b7b7c190b11d
gambi <35289169+blckpeppa@users.noreply.github.com>
2022-11-03 19:39:49 +0300

    cpp q3
    tax payable and net salary calculator

---
+++ s tax payable and net salary calculator.cpp
@@ -0,0 +1,28 @@
+//program calculates tax payable and net salary of employee
+//paye is 25% of gross salary or 0.25
+
+#include <iostream>
+
+using namespace std;
+int main(){
+	// variable declaration
+	int gross_salary;
+	float net_salary;
+	
+	//set gross salary as constant
+	const gross_salary = 10000;
+	//set PAYE as constant
+	const PAYE = 0.25 * gross_salary;
+
+	
+	cout << "This program calculates tax payable and net salary of employee \n";
+	cout << "The tax payable is: "<< PAYE << "\n";
+	
+	//calculate net salary (amount after 25% tax deduction)
+	net_salary = gross_salary - PAYE ; 
+	
+	cout << "Net salary after 25% tax deduction is: "<< net_salary<< "\n";
+	
+	return 0;
+
+}
